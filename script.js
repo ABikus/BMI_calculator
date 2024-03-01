316ee15e134d612ab2a2689805c785b72ac0c44d
AironasBikus <airobikus@gmail.com>
2024-03-01 12:39:39 +0300

    1

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 function calculateBmi() {
    
-    let weight = document.getElementById('weight').value
-    let height = document.getElementById('height').value
+    let weight = parseFloat(document.getElementById('weight').value);
+    let height = parseFloat(document.getElementById('height').value);
+
 
     let bmi = (weight / (height * height))
 
    document.getElementById('heading').innerHTML = 'Your BMI is: '
-   document.getElementById('bmi-output').innerHTML = bmi
+   document.getElementById('bmi-output').innerHTML = bmi.toFixed(2)
 
-
+   if (bmi <= 18.5) {
+    document.getElementById('bmi-output').style.color = 'yellow';
+} else if (bmi >= 18.5 && bmi <= 24.9) {
+    document.getElementById('bmi-output').style.color = 'green';
+} else {
+    document.getElementById('bmi-output').style.color = 'red';
+}
+    
+document.getElementById('clearBtn').style.display = 'inline';
+}
+function reload() {
+    document.getElementById('weight').value = '';
+    document.getElementById('height').value = '';
+    document.getElementById('heading').innerHTML = '';
+    document.getElementById('bmi-output').innerHTML = '';
+    document.getElementById('clearBtn').style.display = 'none';
 }
